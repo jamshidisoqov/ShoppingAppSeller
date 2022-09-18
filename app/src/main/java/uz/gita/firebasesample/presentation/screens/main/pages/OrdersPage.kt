a5407c145f0f8da1c369492ca3319eada79b5b18
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-18 16:52:07 +0500

    orders screen finished

--- app/src/main/java/uz/gita/firebasesample/presentation/screens/main/pages/OrdersPage.kt
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/main/pages/OrdersPage.kt
@@ -1,9 +1,42 @@
 package uz.gita.firebasesample.presentation.screens.main.pages
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import by.kirich1409.viewbindingdelegate.viewBinding
+import kotlinx.coroutines.flow.collectLatest
 import uz.gita.firebasesample.R
+import uz.gita.firebasesample.databinding.PageOrdersBinding
+import uz.gita.firebasesample.presentation.screens.adapter.OrderAdapter
+import uz.gita.firebasesample.presentation.viewmodel.OrdersViewModel
+import uz.gita.firebasesample.presentation.viewmodel.impl.OrdersViewModelImpl
 
 // Created by Jamshid Isoqov an 9/18/2022
 class OrdersPage : Fragment(R.layout.page_orders) {
+
+    private val viewModel: OrdersViewModel by viewModels<OrdersViewModelImpl>()
+
+    private val viewBinding: PageOrdersBinding by viewBinding()
+
+    private val adapter: OrderAdapter by lazy {
+        OrderAdapter()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewBinding.listOrders.adapter = adapter
+
+        adapter.setUpdateListener {
+            viewModel.updateOrder(it)
+        }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.allOrders.collectLatest {
+                adapter.submitList(it)
+            }
+        }
+
+    }
 
 }
