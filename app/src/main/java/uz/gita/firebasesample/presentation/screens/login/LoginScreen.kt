5e21e26d893630734c7bb11cbe23b8c6cde3c39d
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-18 16:21:54 +0500

    login screen finished

--- app/src/main/java/uz/gita/firebasesample/presentation/screens/login/LoginScreen.kt
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/login/LoginScreen.kt
@@ -1,10 +1,41 @@
 package uz.gita.firebasesample.presentation.screens.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import uz.gita.firebasesample.R
+import uz.gita.firebasesample.databinding.ScreenLoginBinding
+import uz.gita.firebasesample.presentation.viewmodel.LoginViewModel
+import uz.gita.firebasesample.presentation.viewmodel.impl.LoginViewModelImpl
 
 // Created by Jamshid Isoqov an 9/18/2022
 class LoginScreen : Fragment(R.layout.screen_login) {
 
+    private val viewModel: LoginViewModel by viewModels<LoginViewModelImpl>()
 
+    private val viewBinding: ScreenLoginBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewBinding.btnLogin.setOnClickListener {
+            viewModel.login(
+                viewBinding.inputLogin.text.toString(),
+                viewBinding.inputPassword.text.toString()
+            )
+        }
+        lifecycleScope.launch {
+            viewModel.errorFlow.collectLatest {
+                Snackbar.make(
+                    viewBinding.btnLogin,
+                    "Login or Password incorrect",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
 }
