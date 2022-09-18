b29fc0bc64006db739654fedb2b84608d7f20f29
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-09-18 13:21:02 +0500

    -add category screen xml and minor change in add category screen

--- app/src/main/java/uz/gita/firebasesample/presentation/screens/add_category/AddCategory.kt
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/add_category/AddCategory.kt
@@ -1,8 +1,17 @@
 package uz.gita.firebasesample.presentation.screens.add_category
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.gita.firebasesample.R
+import uz.gita.firebasesample.databinding.ScreenAddCategoryBinding
 
 // Created by Jamshid Isoqov an 9/18/2022
-class AddCategory:Fragment (R.layout.screen_add_category) {
+class AddCategory : Fragment(R.layout.screen_add_category) {
+    private val viewBinding: ScreenAddCategoryBinding by viewBinding(ScreenAddCategoryBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
 }
