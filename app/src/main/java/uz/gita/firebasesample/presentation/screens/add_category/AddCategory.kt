373b25c4412ebd4b40387f56f3d197aeccdf51b6
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-09-18 13:34:54 +0500

    -minor fix

--- app/src/main/java/uz/gita/firebasesample/presentation/screens/add_category/AddCategory.kt
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/add_category/AddCategory.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.github.drjacky.imagepicker.ImagePicker
+import com.github.drjacky.imagepicker.constant.ImageProvider
 import uz.gita.firebasesample.R
 import uz.gita.firebasesample.databinding.ScreenAddCategoryBinding
 
@@ -13,5 +15,10 @@ class AddCategory : Fragment(R.layout.screen_add_category) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+//        ImagePicker.with(requireActivity())
+//            .provider(ImageProvider.BOTH)
+//            .cropSquare()
+//            .createIntentFromDialog { launcher.launch(it) }
     }
+
 }
