41f203ebb4ecc9db238bcc7667aca5f7692531e8
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-09-18 17:01:51 +0500

    -annotation added

--- app/src/main/java/uz/gita/firebasesample/presentation/screens/products/details/ProductDetailsScreen.kt
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/products/details/ProductDetailsScreen.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
+import dagger.hilt.android.AndroidEntryPoint
 import uz.gita.firebasesample.R
 import uz.gita.firebasesample.databinding.ScreenProductDetailsBinding
 
 // Created by Jamshid Isoqov an 9/18/2022
+@AndroidEntryPoint
 class ProductDetailsScreen : Fragment(R.layout.screen_product_details) {
     private val viewBinding: ScreenProductDetailsBinding by viewBinding(ScreenProductDetailsBinding::bind)
     private val args: ProductDetailsScreenArgs by navArgs()
