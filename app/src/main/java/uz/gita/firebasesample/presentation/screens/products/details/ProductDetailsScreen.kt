e090072160baeb1499bad3b1b586760638936ca3
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-18 15:43:59 +0500

    product details added

---
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/products/details/ProductDetailsScreen.kt
@@ -0,0 +1,8 @@
+package uz.gita.firebasesample.presentation.screens.products.details
+
+import androidx.fragment.app.Fragment
+import uz.gita.firebasesample.R
+
+// Created by Jamshid Isoqov an 9/18/2022
+class ProductDetailsScreen : Fragment(R.layout.screen_product_details) {
+}
