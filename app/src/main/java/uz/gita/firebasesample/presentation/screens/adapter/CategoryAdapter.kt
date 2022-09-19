ef8265ff0eb93272b632648ed8acdaf3d7922dca
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-09-19 18:09:56 +0500

    -bug fixed

--- app/src/main/java/uz/gita/firebasesample/presentation/screens/adapter/CategoryAdapter.kt
+++ app/src/main/java/uz/gita/firebasesample/presentation/screens/adapter/CategoryAdapter.kt
@@ -1,14 +1,18 @@
 package uz.gita.firebasesample.presentation.screens.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import uz.gita.firebasesample.R
 import uz.gita.firebasesample.data.models.local.ProductCategoryData
+import java.lang.Exception
 
 class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.Holder>() {
 
@@ -20,6 +24,7 @@ class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.Holder>() {
         itemClickListener = block
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun submit(list: List<ProductCategoryData>) {
         data.clear()
         data.addAll(list)
@@ -38,8 +43,15 @@ class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.Holder>() {
         fun onBind() {
             val image = itemView.findViewById<ImageView>(R.id.image_category)
             val name = itemView.findViewById<TextView>(R.id.category_name)
+            val progress = itemView.findViewById<ProgressBar>(R.id.progress)
 
-            Picasso.get().load(data[absoluteAdapterPosition].imageUrl).into(image)
+            Picasso.get().load(data[absoluteAdapterPosition].imageUrl)
+                .into(image, object : Callback {
+                    override fun onSuccess() {
+                        progress.visibility = View.GONE
+                    }
+                    override fun onError(e: Exception?) {}
+                })
 
             name.text = data[absoluteAdapterPosition].name
         }
